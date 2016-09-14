4587516e1f76c234a5729086d7835b10b2c7b45e
wochap <gean.marroquin@gmail.com>
2016-09-14 17:24:11 -0500

    update api

--- src-docs/app/pages/HomeApiPage.vue
+++ src-docs/app/pages/HomeApiPage.vue
@@ -4,7 +4,15 @@
     <div class="c-item u-gutter">
       <h4 class="c-item__heading">OffCanvas</h4>
       <h5 class="c-item__sub-heading">Props</h5>
-      <app-table class="c-item__table" :data="tables.props"></app-table>
+      <app-table class="c-item__table" :data="tables.offCanvas.props"></app-table>
+      <h5 class="c-item__sub-heading">Events</h5>
+      <app-table class="c-item__table" :data="tables.offCanvas.events"></app-table>
+      <h4 class="c-item__heading">OffCanvasWrap</h4>
+      <div class="c-item__description">
+        <p>OffCanvasWrap will push its content, when an OffCanvas references it and it is open.</p>
+      </div>
+      <h5 class="c-item__sub-heading">Props</h5>
+      <app-table class="c-item__table" :data="tables.offCanvasWrap.props"></app-table>
     </div>
   </section>
 </template>
@@ -15,14 +23,63 @@
     data () {
       return {
         tables: {
-          props: [
-            {
-              name: 'modifier',
-              type: '`string`',
-              required: 'true',
-              default: '`""`'
-            }
-          ]
+          offCanvasWrap: {
+            props: [
+              {
+                name: 'ref',
+                type: 'String',
+                required: 'true',
+                default: null,
+                description: 'OffCanvasWrap identifier.'
+              }
+            ]
+          },
+          offCanvas: {
+            props: [
+              {
+                name: 'ref',
+                type: 'String',
+                required: 'true',
+                default: null,
+                description: 'OffCanvas identifier.'
+              }, {
+                name: 'class',
+                type: 'String',
+                required: 'false',
+                default: '`false`',
+                description: 'OffCanvas `class` attribute.'
+              }, {
+                name: 'align',
+                type: 'String',
+                required: 'false',
+                default: '`"left"`',
+                description: 'OffCanvas align. It can be `"left"` or `"right"`.'
+              }, {
+                name: 'width',
+                type: 'Number',
+                required: 'false',
+                default: '`300`',
+                description: 'OffCanvas `max-width` style rule.'
+              }, {
+                name: 'wrapRef',
+                type: 'String',
+                required: 'false',
+                default: '`false`',
+                description: 'OffCanvasWrap to push on open OffCanvas.'
+              }
+            ],
+            events: [
+              {
+                name: 'opened',
+                type: 'emitted',
+                description: 'Fired when the OffCanvas is opened. The transition starts.'
+              }, {
+                name: 'closed',
+                type: 'emitted',
+                description: 'Fired when the OffCanvas is closed. The transition ends.'
+              }
+            ]
+          }
         }
       }
     }
