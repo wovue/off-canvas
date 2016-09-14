c6066ad8e1c27e75d33d9e5bfdf12aebc650cfcc
wochap <gean.marroquin@gmail.com>
2016-09-14 18:37:46 -0500

    moving v-transfer-dom outside

--- src-docs/app/components/Root.vue
+++ src-docs/app/components/Root.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <sidebar></sidebar>
-    <router-view></router-view>
-    <gh-corner repo-user="wovue" repo-name="off-canvas" color="#E6E8EA" background="#35495E"></gh-corner>
+    <wv-off-canvas-wrap ref="main">
+      <sidebar></sidebar>
+      <router-view></router-view>
+      <gh-corner v-transfer-dom repo-user="wovue" repo-name="off-canvas" color="#E6E8EA" background="#35495E"></gh-corner>
+    </wv-off-canvas-wrap>
   </div>
 </template>
 
